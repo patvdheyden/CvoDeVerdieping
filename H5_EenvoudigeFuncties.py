c6b9504e0daa315c1bb71e5ac7eb2eece4e2773d
patvdheyden <patvdheyden@gmail.com>
2019-09-26 15:06:47 +0200

    Hoofdstuk 5

--- H5_EenvoudigeFuncties.py
+++ H5_EenvoudigeFuncties.py
@@ -1,9 +1,11 @@
+from typing import Any, Union
+
 from termcolor import colored
 from math import sqrt, exp, log
 import numpy
 import GlobaleFuncties as GF
 import pcinput as PCI
-
+from random  import random
 # Oefening 5.1 #
 print("")
 print("### Oef 5.1: Bereken de lengte van een string ###")
@@ -35,7 +37,27 @@ getal1 = PCI.getFloat("Geef getal 1 :")
 getal2 = PCI.getFloat("Geef getal 2 :")
 getal3 = PCI.getFloat("Geef getal 3 :")
 gegevens = "De drie getallen zijn : " + str(getal1) + " , " + str(getal2) + " , " + str(getal3)
-resultaat_groot = "Het grootste getal is " + str(max(getal1,getal2,getal3))
-resultaat_klein = "Het kleinste getal is " + str(min(getal1,getal2,getal3))
-resultaat_gem = "Het gemiddelde van de getallen is " + str("{:.2f}".format(sum((getal1,getal2,getal3))/3))
-print(GF.box(5, 3, gegevens, resultaat_groot,resultaat_klein,resultaat_gem))
+resultaat_groot = "Het grootste getal is " + str(max(getal1, getal2, getal3))
+resultaat_klein = "Het kleinste getal is " + str(min(getal1, getal2, getal3))
+resultaat_gem = "Het gemiddelde van de getallen is " + str("{:.2f}".format(sum((getal1, getal2, getal3)) / 3))
+print(GF.box(5, 3, gegevens, resultaat_groot, resultaat_klein, resultaat_gem))
+
+# Oefening 5.4 #
+print("")
+print("### Oef 5.4: Berekenen de waardes van e tot de macht x  ###")
+
+
+def expformated(a):
+    resultaat_ef = "De waarde van e tot de macht " + str(a) + " is " + str("{:.5f}".format(exp(a)))
+    return resultaat_ef
+
+
+print(GF.box(5, 4, expformated(-1), expformated(0), expformated(1), expformated(2), expformated(3)))
+
+# Oefening 5.5 #
+print("")
+print("### Oef 5.5: Print een random getal tss van 1 tot 10 met enkel random()  ###")
+
+random_getal = "Een random geheel getal tussen 1 en 10 is " + str(int(random()*10+1))
+
+print(GF.box(5, 5, random_getal))
