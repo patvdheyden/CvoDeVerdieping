a373a069c47836541a282bfe0cfc9af9b0736563
PythonstartersB <patvdheyden@gmail.com>
2019-10-02 19:09:58 +0200

    les4

---
+++ H4_Variabelen/OmzettenFahrenheit.py
@@ -0,0 +1,4 @@
+from termcolor import colored
+fahrenheit = 50
+celsius = (fahrenheit-32)/1.8
+print (colored(str(fahrenheit),'red') +" graden fahrenheit = " +colored(str(celsius),'green') + " graden celsius")
