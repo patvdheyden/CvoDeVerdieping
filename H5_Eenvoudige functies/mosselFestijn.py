6f7dc7f0d25a3e7a73926628dd183d9e970d139f
patvdheyden <patvdheyden@gmail.com>
2019-10-07 12:22:44 +0200

    Hoofdstuk 5

--- H5_Eenvoudige functies/mosselFestijn.py
+++ H5_Eenvoudige functies/mosselFestijn.py
@@ -1,14 +1,15 @@
 import GlobaleFuncties as GF
+import pcinput as PC
 
 prijs_mossel_met_frietjes = 20
 prijs_koninginne_hapje = 10
 prijs_ijs = 3
 prijs_drank = 2
 
-aantal_mossel_met_frietjes = input("Geef het aantal mossem met frietjes in: ")
-aantal_koninginne_hapje = input("Geef het aantal koninginnehapjes in: ")
-aantal_ijs = input("Geef het aantal ijsjes in: ")
-aantal_drank = input("Geef het aantal duvels in: ")
+aantal_mossel_met_frietjes = PC.getInteger("Geef het aantal mossem met frietjes in: ")
+aantal_koninginne_hapje = PC.getInteger("Geef het aantal koninginnehapjes in: ")
+aantal_ijs = PC.getInteger("Geef het aantal ijsjes in: ")
+aantal_drank = PC.getInteger("Geef het aantal duvels in: ")
 
 aantal_mossel_met_frietjes = int(aantal_mossel_met_frietjes)
 aantal_koninginne_hapje = int(aantal_koninginne_hapje)
@@ -28,7 +29,7 @@ str_mosselen = str_format.format("Aantal mosselen-friet", aantal_mossel_met_frie
 str_kon_hapje = str_format.format("Aantal koninginnehapje", aantal_koninginne_hapje , aantal_koninginne_hapje * prijs_koninginne_hapje)
 str_drank = str_format.format("Aantal drankjes", aantal_drank , aantal_drank * prijs_drank)
 str_ijs = str_format.format("Aantal ijs", aantal_ijs , aantal_ijs * prijs_ijs)
-str_te_betalen = "{:<38}{:>8} euro".format("Totaal te betalen : " , totaal_te_betalen)
+str_te_betalen = str_format.format("Totaal te betalen : ","" , totaal_te_betalen)
 str_lijn = "{:<41}{:>10}".format(".", "==========")
 
 print(GF.box(5,6, str_mosselen, str_kon_hapje, str_drank, str_ijs,str_lijn , str_te_betalen ))
@@ -64,14 +65,16 @@ rest_5_euro = rest_10_euro % 5
 resultaat_2_euro = rest_5_euro // 2
 resultaat_1_euro = rest_5_euro % 2
 
-str_resultaat_200 = "Terug te geven 200 euro: " + str(resultaat_200_euro)
-str_resultaat_100 = "Terug te geven 100 euro: " + str(resultaat_100_euro)
-str_resultaat_50 = "Terug te geven 50 euro: " + str(resultaat_50_euro)
-str_resultaat_20 = "Terug te geven 20 euro: " + str(resultaat_20_euro)
-str_resultaat_10 = "Terug te geven 10 euro: " + str(resultaat_10_euro)
-str_resultaat_5 = "Terug te geven 5 euro: " + str(resultaat_5_euro)
-str_resultaat_2 = "Terug te geven 2 euro: " + str(resultaat_2_euro)
-str_resultaat_1 = "Terug te geven 1 euro: " + str(resultaat_1_euro)
+str_format = "Terug te geven {:<3} euro: {:>3}"
+
+str_resultaat_200 = str_format.format(200, resultaat_200_euro)
+str_resultaat_100 = str_format.format(100, resultaat_100_euro)
+str_resultaat_50 = str_format.format(50, resultaat_50_euro)
+str_resultaat_20 = str_format.format(20, resultaat_20_euro)
+str_resultaat_10 = str_format.format(10, resultaat_10_euro)
+str_resultaat_5 = str_format.format(5, resultaat_5_euro)
+str_resultaat_2 = str_format.format(2, resultaat_2_euro)
+str_resultaat_1 = str_format.format(1, resultaat_1_euro)
 
 
 print(GF.box(5,6, str_resultaat_200, str_resultaat_100, str_resultaat_50,str_resultaat_20,str_resultaat_10,str_resultaat_5,str_resultaat_2,str_resultaat_1 ))
