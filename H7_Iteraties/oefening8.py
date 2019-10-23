fe16a17327047ca3d28bd00e84153a982d912841
PythonstartersB <patvdheyden@gmail.com>
2019-10-23 21:39:00 +0200

    H7

--- H7_Iteraties/oefening8.py
+++ H7_Iteraties/oefening8.py
@@ -2,7 +2,7 @@ from random import randint
 from pcinput import getInteger
 cijfer_percent = 0
 resultaat_str = ""
-for i in range(10):
+for i in range(5):
     cijfer1 = randint(0, 1000)
     cijfer2 = randint(0, 1000)
     som = cijfer1 + cijfer2
@@ -10,7 +10,7 @@ for i in range(10):
     resultaat_str += antwoord_str + str(som) + "\n"
     antwoord = getInteger(antwoord_str + "?")
     if int(antwoord) == som:
-        cijfer_percent += 10
+        cijfer_percent += 20
 print ("")
 print(resultaat_str)
 print("U heeft", cijfer_percent, "% behaald")
