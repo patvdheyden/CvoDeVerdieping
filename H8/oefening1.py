1a3b7b9496bf37a72e166249488796477a9d8e43
PythonstartersB <patvdheyden@gmail.com>
2019-11-06 21:49:14 +0100

    H8

--- H8/oefening1.py
+++ H8/oefening1.py
@@ -1,12 +1,16 @@
 from pcinput import getInteger
 
-intGetal = getInteger("Geef een getal kleiner of gelijk aan 10: ")
-printRegel =""
-for i in range(1,intGetal+1):
+intGetal = 1
 
-    for j in range(1,intGetal+1):
-        printRegel += "  {:8} |".format(str(j)+"x"+str(i)+" = "+ str(i*j))
+while intGetal != 0:
 
-    print(printRegel)
-    printRegel = ""
+    intGetal = getInteger("Geef een getal kleiner of gelijk aan 10 (0 voor stop): ")
+    printRegel =""
+    for i in range(1,intGetal+1):
+
+        for j in range(1,intGetal+1):
+            printRegel += "  {:8} |".format(str(j)+"x"+str(i)+" = "+ str(i*j))
+
+        print(printRegel)
+        printRegel = ""
 
