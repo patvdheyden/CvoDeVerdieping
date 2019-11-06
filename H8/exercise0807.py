963c47bf9da8887126a6270851295d29b6331c22
PythonstartersB <patvdheyden@gmail.com>
2019-11-06 21:15:47 +0100

    H8

---
+++ H8/exercise0807.py
@@ -0,0 +1,8 @@
+# What is wrong?
+def area_of_triangle(bottom, height):
+    area = 0.5 * bottom * height
+    print("The area of a triangle with a bottom of", bottom,"and a height of", height, "is", area)
+
+
+# print(area_of_triangle(4.5, 1.0))
+area_of_triangle(4.5, 1.0)
---
+++ H8/scratch.py
@@ -0,0 +1,7 @@
+def print3():
+    print(3)
+
+
+x =  print3()
+
+print (None != x )
